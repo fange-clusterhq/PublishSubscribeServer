1b2c1193451b29c839d5c4905bef3e60d70645b4
Yihua Fang <fang.yihua.eric@gmail.com>
2016-05-02 08:46:29 -0700

    Add documentations for the server class.

--- src/server/server.h
+++ src/server/server.h
@@ -1,3 +1,12 @@
+/*
+ * @file server/server.h
+ * @brief Definitions for the server base class.
+ *
+ * This contains the definitions for a server base class that can be
+ * used for all other types of server implementation.
+ *
+ * @author Yihua Eric Fang (yihuaf)
+ */
 #pragma once
 
 #include <map>
@@ -13,36 +22,118 @@
 
 using namespace std;
 
-static const int MAX_NUM_CONNECTION = 3;
+static const int MAX_NUM_CONNECTION = 64;
+/* Ideally, a HTTP header should not exceeds this length. */
 static const int MAX_MSG_BUFFER_SIZE = 4096;
 
 class Server {
    public:
+      /* @brief Constructor.
+       *
+       * @param port The port this server will listen to.
+       * @return None.
+       */
+      Server(int port);
+      /* @brief Destructor.
+       *
+       * @return None.
+       */
+      ~Server();
+
+      /* @brief Initialize the server.
+       *
+       * We choose not to initialize everything in the Constructor because
+       * we want to avoid throwing exceptions in the case of an error inside
+       * a constructor.
+       *
+       * @return None.
+       */
+      void Init();
+      /* @brief Start the server and serving forever.
+       *
+       * @return None.
+       */
+      void Start();
+   private:
       class Request {
          public:
+            /* This request is received from thsi client. */
             int clientFd;
             char recvBuffer[MAX_MSG_BUFFER_SIZE];
             int numBytes;
 
             Request();
+            /* @brief Return a pointer to the buffer for receiving new data.
+             *
+             * The pointer returned is the position of where the current buffer
+             * is. There might be data stored from the last receive and is only
+             * a particial valid request.
+             *
+             * @return None.
+             */
             inline char *GetBufferForRecv();
+            /* @brief Return the size available in the buffer.
+             *
+             * As mentioned before, the buffer may have stored some data
+             * already, so we return the size currently available.
+             *
+             * @return None.
+             */
             inline size_t GetSizeForRecv();
       };
 
-      Server(int port);
-      ~Server();
-
-      void Init();
-      void Start();
-   private:
+      /* Listening to this port. */
       int port;
+      /* Fd sets for the select(). */
       fd_set readfds;
       fd_set writefds;
+      /* Socket used for the server to accept new connections. */
       int masterSocket;
+      /*
+       * Store the client information for each of the client. The clientFd
+       * should be present in the map if the connection is not closed. The
+       * request should not be NULL us previously this client sent a partial
+       * request.
+       */
       map<int, Request *> clientContext;
 
+      /* @brief Accept a new connection.
+       *
+       * To accept a new connection, we store the fd into the clientContext.
+       *
+       * @return None.
+       */
       void AcceptConnection();
+
+      /* @brief Handle an incoming request.
+       *
+       * When a new fd is ready for read, this method is called to received the
+       * data and process it. If the request cannot be processed, it will store
+       * tha partial request into the clientContent.
+       *
+       * @return None.
+       */
       void HandleRequest(int clientFd);
+
+      /* @brief Prepare the fd sets for the select().
+       *
+       * The select() function require the fd sets to be reinitialized each
+       * time it is being called. Therefore, we need to put all the fds into
+       * the set.
+       *
+       * @return None.
+       */
       int PrepareSelect();
+
+      /* @brief Handle the request after the data is received.
+       *
+       * This function is a pure abstract function which the derived class
+       * of this class should implement. The function should do the heavy
+       * lifting to process the data received and determine if the current
+       * request is full or partial.
+       *
+       * @params request The request containing the data received.
+       * @return Is this request done or only a partial.
+       */
       virtual bool HandleRequestInt(Request *request) = 0;
 };
