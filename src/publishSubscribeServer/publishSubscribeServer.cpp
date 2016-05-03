da53234a88866a10e1afa961fba785a5b2a8480e
Yihua Fang <fang.yihua.eric@gmail.com>
2016-05-02 21:22:53 -0700

    Add place holder for the API.

--- src/publishSubscribeServer/publishSubscribeServer.cpp
+++ src/publishSubscribeServer/publishSubscribeServer.cpp
@@ -6,6 +6,8 @@
 
 #include "publishSubscribeServer.h"
 
+using namespace std;
+
 PublishSubscribeServer::PublishSubscribeServer(int port)
    :Server(port)
 {}
@@ -18,5 +20,29 @@ bool
 PublishSubscribeServer::HandleRequestInt(Request *request)
 {
    return true;
+}
+
+void
+PublishSubscribeServer::Subscribe(string username,
+                                  string topic)
+{
+}
+
+void
+PublishSubscribeServer::Unsubscribe(string username,
+                                    string topic)
+{
+}
+
+void
+PublishSubscribeServer::Publish(string topic,
+                                char *msg)
+{
+}
+
+void
+PublishSubscribeServer::GetNextMessage(string username,
+                                       string topic)
+{
 }
 
--- src/publishSubscribeServer/publishSubscribeServer.h
+++ src/publishSubscribeServer/publishSubscribeServer.h
@@ -1,6 +1,38 @@
 #pragma once
 
 #include <server/server.h>
+#include <string>
+#include <map>
+#include <queue>
+#include "publishSubscribeRequest.h"
+
+using namespace std;
+
+/*
+typedef struct PublishMsg {
+   char *msg;
+   int numBytes;
+   int refCount;
+} PublishMsg;
+
+bool FreePublishMsg(PublishMsg *msg) {
+   msg->refCount--;
+   if (msg->refCount == 0) {
+      delete msg->msg;
+   }
+}
+*/
+
+class PublishSubscribeSystem {
+   public:
+      void Subscribe(string &username, string &topic);
+      void Unsubscribe(string &username, string &topic);
+      void Publish(string &topic, char *msg);
+      void GetNextMessage(string &username, string &topic);
+   private:
+      map<string, string> topicSubscription;
+      map<string, queue<PublishedMsg *>> msgQueue;
+};
 
 class PublishSubscribeServer : public Server {
    public:
@@ -8,10 +40,4 @@ class PublishSubscribeServer : public Server {
       ~PublishSubscribeServer();
    private:
       bool HandleRequestInt(Request *request);
-      /*
-      void Subscribe();
-      void Unsubscribe();
-      void Publish();
-      void GetNextMessage();
-      */
 };
